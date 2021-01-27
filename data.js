6ba915ba76180e8aa4cba5f7196654fba3ad97ab
Harry Bond-Preston <harrybond-preston@Harrys-MacBook-Air.local>
2021-01-27 18:57:56 +0000

    Bring in asciiSite update

---
+++ data.js
@@ -0,0 +1,36 @@
+let my_site = {
+    "title_light": "<a onclick='site.toggle_dark_mode()'>^-^</a> harrycats",
+    "title_dark": "<a onclick='site.toggle_dark_mode()'>o-o</a> harrycats",
+    "pages": [
+    {
+        "title": "Projects",
+        "tabs": [
+        {
+            "title": "Games",
+            "description": "Game Projects",
+            "contents": "Various Games made by me over the last few years in my spare time. All were made completely from scratch either in pure HTML5 with javascript and the canvas or in C++ and compiled to web assembly. All should run in the browser. Only a couple of them work with mobile devices.<br><br><b>Cave Escape (2019 - Present)</b><br> Avoid the monsters and try to escape the vast cave system. Collect as many coins as possible. Created in C++ and compiled to web-assembly using emscripten. Still a work in progress.<br> <a href='games/cave_escape.html'>Click</a> to play<br><br><b>Ocean Simulator (2018)</b><br> Play with fishes and <a onclick='open_popover(2)'>fish-eating worms</a> in this fun little sandbox. This is an implementation of boids written in javascript.<br> <a href='games/ocean_simulator.html'>Click</a> to play<br><br><b>Bounce (2018)</b><br> Sort of like 2D single-player pong? Keep the ball from escaping! Created in javascript - works on mobile.<br> <a href='games/bounce.html'>Click</a> to play<br><br><b>Zombie Run (2017)</b><br> A side scrolling run-and-shoot type game with randomly generated caves. See how far you can get! Created in javascript - works on mobile.<br> <a href='games/zombie_run.html'>Click</a> to play<br><br><b>Meteor Shower (2017)</b><br> Dodge falling blocks and collect health cubes as you try to survive for as long as possible. Created in javascript.<br> <a href='games/meteor_shower.html'>Click</a> to play<br><br><b>Endless Climb (2017)</b><br> Race against time as you jump upwards from block to block in this fun little concept game. Created in javascript.<br> <a href='games/endless_climb.html'>Click</a> to play<br><br>Check my <a href='https://github.com/harrybp'>github</a> for more stuff"
+        },
+        {
+            "title": "Other",
+            "description": "Other Projects",
+            "contents": "<b>Texture Generation using ML (2018)</b><br> Created as part of my final year project at uni, a demonstration of a few methods of synthesising unique textures using machine learning methods. <br> See the <a href='https://harrybp.github.io/texture_generation_demo/'>demo</a> or check it out on <a href='https://github.com/harrybp/TextureGeneration'>github</a><br><br><b>This website (2017 - Present)</b><br> A text-only interactive website built using javascript with support for a navbar, tabs, pop-ups, pop-overs and columns of text. Hint: try clicking the cat face in the nav bar! <br> Check it out on <a href='https://github.com/harrybp/asciiSite'>github</a>"
+        }
+        ]
+    },
+    {
+        "title": "About Me",
+        "tabs": [
+        {
+            "title": "Info",
+            "description": "Site Information",
+            "contents": "Hi I'm Harry!<br><br>This website serves as an archive for all of the web projects I have worked on over the years. The stuff on here is all for fun - I have been trying to learn game development so a lot of the projects are little games. Feel free to check them out and contact me with any feedback."
+        },
+        {
+            "title": "Contact",
+            "description": "Contact Information",
+            "contents": "You can email me at <a href='mailto:harrybp@me.com'>harrybp@me.com</a> or checkout my github at <a href='https://github.com/harrybp'>github.com/harrybp</a>"
+        }
+        ]
+    }
+    ]
+}
